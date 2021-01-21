a14fffebb2e7e26de0baf1476d65098f70cc3021
Renan Rossignatti de França <renanrfranca@gmail.com>
2021-01-20 22:39:11 -0300

    feat: skeleton for the menu

--- src/com/renan/customMap/Main.java
+++ src/com/renan/customMap/Main.java
@@ -1,8 +1,47 @@
 package com.renan.customMap;
 
+import java.util.Scanner;
+
 public class Main {
+    private final static CustomMap<String, String> map = new CustomMap<>();
 
     public static void main(String[] args) {
-	// write your code here
+        int option = -1;
+        Scanner in = new Scanner(System.in);
+
+        do {
+            try {
+                printMainMenu();
+                option = Integer.parseInt(in.nextLine());
+                callAction(option);
+            } catch (NumberFormatException e) {
+                System.out.println("Opção inválida");
+            }
+        } while (option != 0);
+    }
+
+    private static void printMainMenu() {
+        System.out.println("Escolha uma opção abaixo: ");
+        System.out.println("1 - adicionar item");
+        System.out.println("2 - buscar item");
+        System.out.println("3 - remover item");
+        System.out.println("0 - Sair");
+    }
+
+    private static void callAction(int actionCode) {
+        switch (actionCode) {
+            case 1:
+                System.out.println("Adição");
+                break;
+            case 2:
+                System.out.println("Busca");
+                break;
+            case 3:
+                System.out.println("Remoção");
+                break;
+            case 0:
+                System.out.println("Até mais!");
+                break;
+        }
     }
 }
